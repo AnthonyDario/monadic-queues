8dccec51a7e0d8c3fd756390eac4047c53ccc76a
Anthony Dario <anthonyrdario@gmail.com>
2023-05-07 16:33:12 -0700

    Send rabbitmq message via http request

--- server.go
+++ server.go
@@ -1,16 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type handler func(http.ResponseWriter, *http.Request)
+
+// Util
+// ---------------------
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+// Handlers
+// ---------------------
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func makeSendHandler(q amqp.Queue, ch *amqp.Channel, ctx context.Context) handler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hi there, I am sending your message!")
+
+		body := "Hello World!"
+		err := ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+	}
+}
+
+// Setup
+// ---------------------
+func connect() (*amqp.Connection, *amqp.Channel, amqp.Queue) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:53098/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	return conn, ch, q
+}
 func main() {
-	http.HandleFunc("/", handler)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, ch, q := connect()
+	defer conn.Close()
+	defer ch.Close()
+
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/send", makeSendHandler(q, ch, ctx))
+	log.Print("Starting Server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
