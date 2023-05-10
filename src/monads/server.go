4b3f1421cf0b7ceff01560db8b29e8664b9de3c4
Anthony Dario <anthonyrdario@gmail.com>
2023-05-10 13:12:54 -0700

    Split monad server into multiple files

--- src/monads/server.go
+++ src/monads/server.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type handler func(http.ResponseWriter, *http.Request)
@@ -27,7 +23,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func makeSendHandler(q amqp.Queue, ch *amqp.Channel, ctx context.Context) handler {
+func makeSendHandler(q Queue) handler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Print("Recieved Send Request")
 
@@ -39,57 +35,22 @@ func makeSendHandler(q amqp.Queue, ch *amqp.Channel, ctx context.Context) handle
 		reqBody, err := ioutil.ReadAll(r.Body)
 		failOnError(err, "Failed to publish a message")
 
-		err = ch.PublishWithContext(ctx,
-			"",     // exchange
-			q.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(reqBody),
-			})
+        err = q.send([]byte(reqBody))
 		failOnError(err, "Failed to publish a message")
-		fmt.Fprintf(w, "Message Sent")
 
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Message Sent")
 		log.Print("Finished Sending Message")
 	}
 }
 
-// Setup
-// ---------------------
-func connect() (*amqp.Connection, *amqp.Channel, amqp.Queue) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:53098/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	return conn, ch, q
-}
-
 func main() {
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	conn, ch, q := connect()
-	defer conn.Close()
-	defer ch.Close()
+	q := connect("pizza")
+	defer q.Close()
 
 	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/send", makeSendHandler(q, ch, ctx))
+	http.HandleFunc("/send", makeSendHandler(q))
 	log.Print("Starting Server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
