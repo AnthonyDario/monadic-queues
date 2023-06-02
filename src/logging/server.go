875ee94b7cf0c9338f1dcc51597812168837a27e
Anthony Dario <anthonyrdario@gmail.com>
2023-06-02 12:44:47 -0700

    Add ability to retrieve logs

--- src/logging/server.go
+++ src/logging/server.go
@@ -4,9 +4,11 @@ import (
     "log"
     "net/http"
     "fmt"
-	"io/ioutil"
+	"io"
     "os"
 )
+
+const LOG_FILE = "log.log"
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -14,6 +16,7 @@ func failOnError(err error, msg string) {
 	}
 }
 
+// TODO: have the failure return a failure response
 func logHandler (w http.ResponseWriter, r *http.Request) {
     // Write the request body to a log file
     // Lets just do the single log file for now...
@@ -24,21 +27,35 @@ func logHandler (w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    reqBody, err := ioutil.ReadAll(r.Body)
+    reqBody, err := io.ReadAll(r.Body)
     failOnError(err, "Failed to publish a message")
 
     log.Println(string(reqBody))
 }
 
-func main() {
-    LOG_FILE := "log.log"
+// Just returning the entire log file now for now. DOES
+// NOT SCALE
+func getHandler (w http.ResponseWriter, r *http.Request) {
+    logFile, err := os.Open(LOG_FILE)
+    failOnError(err, "Could not open log file for reading")
+    defer logFile.Close()
 
+    bs, err := io.ReadAll(logFile)
+    failOnError(err, "Could not read the log file as bytes")
+
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, string(bs))
+}
+
+func main() {
     logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
     failOnError(err, "Could not open log file")
     defer logFile.Close()
     log.SetOutput(logFile)
 
 	http.HandleFunc("/log", logHandler)
+    http.HandleFunc("/get", getHandler)
     fmt.Print("Starting Log Server");
 	log.Print("Starting Log Server")
 	log.Fatal(http.ListenAndServe(":8000", nil))
--- src/logging/test.go
+++ src/logging/test.go
@@ -7,18 +7,28 @@ import (
 	"net/http"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
 func main() {
 	// Send a post body
 	json := "This is a new log line"
 	body := []byte(json)
 	res, err := http.Post("http://localhost:8000/log", "text/plain", bytes.NewReader(body))
-	if err != nil {
-		log.Fatalf("error with posting the log: %s", err)
-	}
+    failOnError(err, "error with posting the log: %s")
 
 	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalf("impossible to read all body of response: %s", err)
-	}
+    failOnError(err, "impossible to read all body of response")
 	log.Printf("res body: %s", string(resBody))
+
+    // Get the log file
+    res, err = http.Get("http://localhost:8000/get")
+    failOnError(err, "Could not retrieve the log file from the server")
+
+    resBody, err = io.ReadAll(res.Body)
+    failOnError(err, "Could not read log file response")
+    log.Printf("res body:\n%s", string(resBody))
 }
